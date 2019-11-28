0bea894d7964af7437a9256f296db115d9da9b68
Jenny_Ripper <jennifer.ripper@gmail.com>
2019-11-28 12:00:02 -0500

    updated file projess, first 3 images

---
+++ main.js
@@ -0,0 +1,32 @@
+const nasaObject = [ 
+    {
+        "date": "2019-04-05",
+        "explanation": "This astronomical sky spanning view is a mosaic from the Pan-STARRS observatory. The images were recorded with its 1.8 meter telescope at the summit of Haleakala on planet Earth's island of Maui. In fact, Earth's north celestial pole is centered in this across-the-sky projection. A declination of -30 degrees, the southern horizon limit as seen from the Hawaiian Valley Isle, defines the circular outer edge. Crowded starfields and cosmic dust clouds along the plane of our Milky Way galaxy stretch across the scene with the bright bulge of the galactic center at the bottom. Compiled over four years, the image data represent the second edition of data from Pan-STARRS (Panoramic Survey Telescope & Rapid Response System), currently the planet's largest digital sky survey. In 2017 Pan-STARRS was used to first recognize the interstellar voyage of 'Oumuamua, visitor to our Solar System.",
+        "hdurl": "https://apod.nasa.gov/apod/image/1904/STScI-H-1912b-panstarrs.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "Pan-STARRS Across the Sky",
+        "url": "https://apod.nasa.gov/apod/image/1904/STScI-H-1912b-panstarrs1024.jpg"
+    },
+
+    {
+        "copyright": "Robert Eder",
+        "date": "2019-09-21",
+        "explanation": "Framing a bright emission region, this telescopic view looks out across a pretty field of stars along the plane of our Milky Way Galaxy, toward the nebula rich constellation Cygnus the Swan. Popularly called the Tulip Nebula, the reddish glowing cloud of interstellar gas and dust is also found in the 1959 catalog by astronomer Stewart Sharpless as Sh2-101. About 8,000 light-years distant and 70 light-years across the complex and beautiful nebula blossoms near the center of this composite image. Ultraviolet radiation from young energetic O stars at the edge of the Cygnus OB3 association, ionizes the atoms and powers the emission from the Tulip Nebula.",
+        "hdurl": "https://apod.nasa.gov/apod/image/1909/Tulip_CropRobertEder.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "The Tulip in the Swan",
+        "url": "https://apod.nasa.gov/apod/image/1909/Tulip_CropRobertEder1024.jpg"
+    },
+
+    {
+        "date": "2018-12-25",
+        "explanation": "Majestic on a truly cosmic scale, M100 is appropriately known as a grand design spiral galaxy.  It is a large galaxy of over 100 billion stars with well-defined spiral arms that is similar to our own Milky Way Galaxy.  One of the brightest members of the Virgo Cluster of galaxies, M100 (alias NGC 4321) is 56 million light-years distant toward the constellation of Berenice's Hair (Coma Berenices).  This Hubble Space Telescope image of M100 was taken recently with the Wide Field Camera 3 and accentuates bright blue star clusters and intricate winding dust lanes which are hallmarks of this class of galaxies.  Studies of variable stars in M100 have played an important role in determining the size and age of the Universe.",
+        "hdurl": "https://apod.nasa.gov/apod/image/1812/M100_HubbleWfc3_3679.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "M100: A Grand Design Spiral Galaxy",
+        "url": "https://apod.nasa.gov/apod/image/1812/M100_HubbleWfc3_960.jpg"
+    }
+]
