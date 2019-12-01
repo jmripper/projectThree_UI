71960f4a7c9c0c1d5a8f7da1e6e1ec8bd09c03d9
Jenny_Ripper <jennifer.ripper@gmail.com>
2019-12-01 10:17:44 -0500

    new div with image class added

--- main.js
+++ main.js
@@ -1,65 +1,76 @@
-const nasaObject = [ 
+var nasaObjectList = [ 
     {
-        "date": "2019-04-05",
-        "explanation": "This astronomical sky spanning view is a mosaic from the Pan-STARRS observatory. The images were recorded with its 1.8 meter telescope at the summit of Haleakala on planet Earth's island of Maui. In fact, Earth's north celestial pole is centered in this across-the-sky projection. A declination of -30 degrees, the southern horizon limit as seen from the Hawaiian Valley Isle, defines the circular outer edge. Crowded starfields and cosmic dust clouds along the plane of our Milky Way galaxy stretch across the scene with the bright bulge of the galactic center at the bottom. Compiled over four years, the image data represent the second edition of data from Pan-STARRS (Panoramic Survey Telescope & Rapid Response System), currently the planet's largest digital sky survey. In 2017 Pan-STARRS was used to first recognize the interstellar voyage of 'Oumuamua, visitor to our Solar System.",
-        "hdurl": "https://apod.nasa.gov/apod/image/1904/STScI-H-1912b-panstarrs.jpg",
-        "media_type": "image",
-        "service_version": "v1",
-        "title": "Pan-STARRS Across the Sky",
-        "url": "https://apod.nasa.gov/apod/image/1904/STScI-H-1912b-panstarrs1024.jpg"
+        date: "2019-04-05",
+        explanation: "This astronomical sky spanning view is a mosaic from the Pan-STARRS observatory. The images were recorded with its 1.8 meter telescope at the summit of Haleakala on planet Earth's island of Maui. In fact, Earth's north celestial pole is centered in this across-the-sky projection. A declination of -30 degrees, the southern horizon limit as seen from the Hawaiian Valley Isle, defines the circular outer edge. Crowded starfields and cosmic dust clouds along the plane of our Milky Way galaxy stretch across the scene with the bright bulge of the galactic center at the bottom. Compiled over four years, the image data represent the second edition of data from Pan-STARRS (Panoramic Survey Telescope & Rapid Response System), currently the planet's largest digital sky survey. In 2017 Pan-STARRS was used to first recognize the interstellar voyage of 'Oumuamua, visitor to our Solar System.",
+        hdurl: "https://apod.nasa.gov/apod/image/1904/STScI-H-1912b-panstarrs.jpg",
+        media_type: "image",
+        service_version: "v1",
+        title: "Pan-STARRS Across the Sky",
+        url: "https://apod.nasa.gov/apod/image/1904/STScI-H-1912b-panstarrs1024.jpg"
     },
 
     {
-        "copyright": "Robert Eder",
-        "date": "2019-09-21",
-        "explanation": "Framing a bright emission region, this telescopic view looks out across a pretty field of stars along the plane of our Milky Way Galaxy, toward the nebula rich constellation Cygnus the Swan. Popularly called the Tulip Nebula, the reddish glowing cloud of interstellar gas and dust is also found in the 1959 catalog by astronomer Stewart Sharpless as Sh2-101. About 8,000 light-years distant and 70 light-years across the complex and beautiful nebula blossoms near the center of this composite image. Ultraviolet radiation from young energetic O stars at the edge of the Cygnus OB3 association, ionizes the atoms and powers the emission from the Tulip Nebula.",
-        "hdurl": "https://apod.nasa.gov/apod/image/1909/Tulip_CropRobertEder.jpg",
-        "media_type": "image",
-        "service_version": "v1",
-        "title": "The Tulip in the Swan",
-        "url": "https://apod.nasa.gov/apod/image/1909/Tulip_CropRobertEder1024.jpg"
+        copyright: "Robert Eder",
+        date: "2019-09-21",
+        explanation: "Framing a bright emission region, this telescopic view looks out across a pretty field of stars along the plane of our Milky Way Galaxy, toward the nebula rich constellation Cygnus the Swan. Popularly called the Tulip Nebula, the reddish glowing cloud of interstellar gas and dust is also found in the 1959 catalog by astronomer Stewart Sharpless as Sh2-101. About 8,000 light-years distant and 70 light-years across the complex and beautiful nebula blossoms near the center of this composite image. Ultraviolet radiation from young energetic O stars at the edge of the Cygnus OB3 association, ionizes the atoms and powers the emission from the Tulip Nebula.",
+        hdurl: "https://apod.nasa.gov/apod/image/1909/Tulip_CropRobertEder.jpg",
+        media_type: "image",
+        service_version: "v1",
+        title: "The Tulip in the Swan",
+        url: "https://apod.nasa.gov/apod/image/1909/Tulip_CropRobertEder1024.jpg"
     },
 
     {
-        "date": "2018-12-25",
-        "explanation": "Majestic on a truly cosmic scale, M100 is appropriately known as a grand design spiral galaxy.  It is a large galaxy of over 100 billion stars with well-defined spiral arms that is similar to our own Milky Way Galaxy.  One of the brightest members of the Virgo Cluster of galaxies, M100 (alias NGC 4321) is 56 million light-years distant toward the constellation of Berenice's Hair (Coma Berenices).  This Hubble Space Telescope image of M100 was taken recently with the Wide Field Camera 3 and accentuates bright blue star clusters and intricate winding dust lanes which are hallmarks of this class of galaxies.  Studies of variable stars in M100 have played an important role in determining the size and age of the Universe.",
-        "hdurl": "https://apod.nasa.gov/apod/image/1812/M100_HubbleWfc3_3679.jpg",
-        "media_type": "image",
-        "service_version": "v1",
-        "title": "M100: A Grand Design Spiral Galaxy",
-        "url": "https://apod.nasa.gov/apod/image/1812/M100_HubbleWfc3_960.jpg"
+        date: "2018-12-25",
+        explanation: "Majestic on a truly cosmic scale, M100 is appropriately known as a grand design spiral galaxy.  It is a large galaxy of over 100 billion stars with well-defined spiral arms that is similar to our own Milky Way Galaxy.  One of the brightest members of the Virgo Cluster of galaxies, M100 (alias NGC 4321) is 56 million light-years distant toward the constellation of Berenice's Hair (Coma Berenices).  This Hubble Space Telescope image of M100 was taken recently with the Wide Field Camera 3 and accentuates bright blue star clusters and intricate winding dust lanes which are hallmarks of this class of galaxies.  Studies of variable stars in M100 have played an important role in determining the size and age of the Universe.",
+        hdurl: "https://apod.nasa.gov/apod/image/1812/M100_HubbleWfc3_3679.jpg",
+        media_type: "image",
+        service_version: "v1",
+        title: "M100: A Grand Design Spiral Galaxy",
+        url: "https://apod.nasa.gov/apod/image/1812/M100_HubbleWfc3_960.jpg"
     },
 
     {
-        "copyright": "Francesco Antonucci",
-        "date": "2014-10-01",
-        "explanation": "The bright clusters and nebulae of planet Earth's night sky are often named for flowers or insects. Though its wingspan covers over 3 light-years, NGC 6302 is no exception. With an estimated surface temperature of about 250,000 degrees C, the dying central star of this particular planetary nebula has become exceptionally hot, shining brightly in ultraviolet light but hidden from direct view by a dense torus of dust.  This sharp close-up of the dying star's nebula was recorded in 2009 by the Hubble Space Telescope's Wide Field Camera 3, and is presented here in reprocessed colors.  Cutting across a bright cavity of ionized gas, the dust torus surrounding the central star is near the center of this view, almost edge-on to the line-of-sight. Molecular hydrogen has been detected in the hot star's dusty cosmic shroud. NGC 6302 lies about 4,000 light-years away in the arachnologically correct constellation of the Scorpion (Scorpius).    APOD Wall Calendar: Nebulas and Star Clusters",
-        "hdurl": "https://apod.nasa.gov/apod/image/1410/butterflyblue_hst_3919.jpg",
-        "media_type": "image",
-        "service_version": "v1",
-        "title": "The Butterfly Nebula from Hubble",
-        "url": "https://apod.nasa.gov/apod/image/1410/butterflyblue_hst_960.jpg"
+        copyright: "Francesco Antonucci",
+        date: "2014-10-01",
+        explanation: "The bright clusters and nebulae of planet Earth's night sky are often named for flowers or insects. Though its wingspan covers over 3 light-years, NGC 6302 is no exception. With an estimated surface temperature of about 250,000 degrees C, the dying central star of this particular planetary nebula has become exceptionally hot, shining brightly in ultraviolet light but hidden from direct view by a dense torus of dust.  This sharp close-up of the dying star's nebula was recorded in 2009 by the Hubble Space Telescope's Wide Field Camera 3, and is presented here in reprocessed colors.  Cutting across a bright cavity of ionized gas, the dust torus surrounding the central star is near the center of this view, almost edge-on to the line-of-sight. Molecular hydrogen has been detected in the hot star's dusty cosmic shroud. NGC 6302 lies about 4,000 light-years away in the arachnologically correct constellation of the Scorpion (Scorpius).    APOD Wall Calendar: Nebulas and Star Clusters",
+        hdurl: "https://apod.nasa.gov/apod/image/1410/butterflyblue_hst_3919.jpg",
+        media_type: "image",
+        service_version: "v1",
+        title: "The Butterfly Nebula from Hubble",
+        url: "https://apod.nasa.gov/apod/image/1410/butterflyblue_hst_960.jpg"
     },
 
     {
-        "date": "2004-06-01",
-        "explanation": "Star formation occurs at a faster pace in M82 -- a galaxy with about ten times the rate of massive star birth (and death) compared to our Milky Way.  Winds from massive stars and blasts from supernova explosions have created a billowing cloud of expanding gas from this remarkable starburst galaxy.  The above scientifically color-coded image highlights the complexity and origin of the plume by combining a wide field image from the WIYN Telescope in Arizona with a smaller high-resolution image from the orbiting Hubble Space Telescope.  M82's aspect in optical pictures has led to its popular moniker, the Cigar Galaxy.  M82's burst of star formation was likely triggered a mere 100 million years ago in the latest of a series of bouts with neighboring large galaxy M81.",
-        "hdurl": "https://apod.nasa.gov/apod/image/0406/m82_hstwiyn_big.jpg",
-        "media_type": "image",
-        "service_version": "v1",
-        "title": "The Supergalactic Wind from Starburst Galaxy M82",
-        "url": "https://apod.nasa.gov/apod/image/0406/m82_hstwiyn.jpg"
+        date: "2004-06-01",
+        explanation: "Star formation occurs at a faster pace in M82 -- a galaxy with about ten times the rate of massive star birth (and death) compared to our Milky Way.  Winds from massive stars and blasts from supernova explosions have created a billowing cloud of expanding gas from this remarkable starburst galaxy.  The above scientifically color-coded image highlights the complexity and origin of the plume by combining a wide field image from the WIYN Telescope in Arizona with a smaller high-resolution image from the orbiting Hubble Space Telescope.  M82's aspect in optical pictures has led to its popular moniker, the Cigar Galaxy.  M82's burst of star formation was likely triggered a mere 100 million years ago in the latest of a series of bouts with neighboring large galaxy M81.",
+        hdurl: "https://apod.nasa.gov/apod/image/0406/m82_hstwiyn_big.jpg",
+        media_type: "image",
+        service_version: "v1",
+        title: "The Supergalactic Wind from Starburst Galaxy M82",
+        url: "https://apod.nasa.gov/apod/image/0406/m82_hstwiyn.jpg"
     },
 
     {
-        "copyright": "Tamas Ladanyi",
-        "date": "2010-08-12",
-        "explanation": "ch August, as planet Earth swings through dust trailing along the orbit of periodic comet Swift-Tuttle, skygazers can enjoy the Perseid Meteor Shower. The shower should build to its peak now, best seen from later tonight after moonset, until dawn tomorrow morning when Earth moves through the denser part of the wide dust trail. But shower meteors have been spotted for many days, like this bright Perseid streaking through skies near Lake Balaton, Hungary on August 8. In the foreground is the region's Church of St. Andrew ruin, with bright Jupiter dominating the sky to its right. Two galaxies lie in the background of the wide-angle, 3 frame panorama; our own Milky Way's luminous arc, and the faint smudge of the more distant Andromeda Galaxy just above the ruin's leftmost wall. If you watch for Perseid meteors tonight, be sure and check out the early evening sky show too, featuring bright planets and a young crescent Moon near the western horizon after sunset.",
-        "hdurl": "https://apod.nasa.gov/apod/image/1008/perseid_100808_ladanyi_v1.jpg",
-        "media_type": "image",
-        "service_version": "v1",
-        "title": "Perseid Prelude",
-        "url": "https://apod.nasa.gov/apod/image/1008/perseid_100808_ladanyi.jpg"
+        copyright: "Tamas Ladanyi",
+        date: "2010-08-12",
+        explanation: "Each August, as planet Earth swings through dust trailing along the orbit of periodic comet Swift-Tuttle, skygazers can enjoy the Perseid Meteor Shower. The shower should build to its peak now, best seen from later tonight after moonset, until dawn tomorrow morning when Earth moves through the denser part of the wide dust trail. But shower meteors have been spotted for many days, like this bright Perseid streaking through skies near Lake Balaton, Hungary on August 8. In the foreground is the region's Church of St. Andrew ruin, with bright Jupiter dominating the sky to its right. Two galaxies lie in the background of the wide-angle, 3 frame panorama; our own Milky Way's luminous arc, and the faint smudge of the more distant Andromeda Galaxy just above the ruin's leftmost wall. If you watch for Perseid meteors tonight, be sure and check out the early evening sky show too, featuring bright planets and a young crescent Moon near the western horizon after sunset.",
+        hdurl: "https://apod.nasa.gov/apod/image/1008/perseid_100808_ladanyi_v1.jpg",
+        media_type: "image",
+        service_version: "v1",
+        title: "Perseid Prelude",
+        url: "https://apod.nasa.gov/apod/image/1008/perseid_100808_ladanyi.jpg"
     }
 ]
 
+console.log(nasaObjectList);
+//const modal = document.getElementById("modalItem");
+let imageList = document.querySelectorAll(".imagelist")
+console.log(imageList)
+
+imageList.forEach((imageItem, i) => {
+    let nasaData = nasaObjectList[i]
+    let imageTitle = nasaData[0].title
+    //console.log(imageTitle)
+    
+})
